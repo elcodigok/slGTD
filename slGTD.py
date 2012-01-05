f90c1e9fb8e43594be7e47276034a0092b8e9de4
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2012-01-05 12:25:04 -0300

    Estructura de menú minimalista

--- slGTD.py
+++ slGTD.py
@@ -22,9 +22,8 @@ import ConfigParser
 import logging
 import os
 import sys
+import gettext
 
-import lib.ping as ping
-import lib.IPy as IP
 import app.default.menu as Menu
 import app.default.ventana as Ventana
 import app.config.ventanaconfig as Configuracion
@@ -44,15 +43,6 @@ logging.basicConfig(level=logging.INFO,
 					filename=path_logging)
 logger = logging.getLogger(path_logging)
 
-path_banned_ip = config.get('Datos', 'banned_ip')
-path_banned_site = config.get('Datos', 'banned_site')
-path_banned_extension = config.get('Datos', 'banned_extension')
-
-path_exception_ip = config.get('Datos', 'exception_ip')
-path_exception_site = config.get('Datos', 'exception_site')
-path_exception_extension = config.get('Datos', 'exception_extension')
-gw = config.get('Datos', 'default_gateway')
-
 file_inbox = config.get('Datos', 'inbox')
 
 def menuPrincipal():
@@ -62,8 +52,9 @@ def menuPrincipal():
 		bb = ButtonBar(formulario, (("Recopilar", "recopilar"),
 								("Procesar", "procesar"),
 								("Organizar", "organizar"),
-								("Revisar", "revisar")))
-		salir = Button("Salir")
+								("Revisar", "revisar"),
+								("Salir", "salir"))
+								)
 		g = GridForm(formulario, "slGTD 0.1", 1, 3)
 		g.add(bb, 0, 0, (2, 2, 2, 2))
 	
@@ -74,108 +65,9 @@ def menuPrincipal():
 									"Tareas.",
 									formulario)
 			rta3 = inboxMenu.mostrarListado()
-
-	salir = False
-	while not salir:
-		primerMenu = Menu.Menu("slGTD 0.1",
-							"Sistema de Administración de Tareas.",
-							("INBOX",
-								"Próximo",
-								"Proyectos",
-								"En espera",
-								"Algún día",
-								"salir"),
-							formulario)
-		rta = primerMenu.mostrarMenu()
-		if (rta == 0):
-			logger.info('INBOX.')
-			exit_sub_menu = False
-			while not exit_sub_menu:
-				inboxMenu = Ventana.Ventana(file_inbox,
-										"INBOX | slGTD",
-										"Tareas.",
-										formulario)
-				rta3 = inboxMenu.mostrarListado()
-				exit_sub_menu = True
-		elif (rta == 1):
-			logger.info('Administración de Dominios.')
-			exit_sub_menu = False
-			while not exit_sub_menu:
-				auxMenu = Menu.Menu("slGuardian | Administrar Dominios",
-								"Administración de Dominios",
-								("Bloquear Dominios",
-								"Liberar Dominios",
-								"Volver"),
-								formulario)
-				rta2 = auxMenu.mostrarMenu()
-				if (rta2 == 0):
-					segundoMenu = Ventana.Ventana(path_banned_site,
-											"slGuardian | Listado de Dominios",
-											"Dominios bloqueados.",
-											formulario)
-					rta3 = segundoMenu.mostrarListado()
-				elif (rta2 == 1):
-					segundoMenu = Ventana.Ventana(path_exception_site,
-											"slGuardian | Listado de Dominios",
-											"Dominios liberados.",
-											formulario)
-					rta3 = segundoMenu.mostrarListado()
-				elif (rta2 == 2):
-					exit_sub_menu = True
-		elif (rta == 2):
-			logger.info('Administración de Extensiones.')
-			exit_sub_menu = False
-			while not exit_sub_menu:
-				auxMenu = Menu.Menu("slGuardian | Administrar Extensiones",
-								"Administración de Extensiones en Archivos",
-								("Bloquear Extensiones",
-								"Liberar Extensiones",
-								"Volver"),
-								formulario)
-				rta2 = auxMenu.mostrarMenu()
-				if (rta2 == 0):
-					segundoMenu = Ventana.Ventana(path_banned_extension,
-											"slGuardian | Listado de Extensiones",
-											"Extensiones bloqueadas.",
-											formulario)
-					rta3 = segundoMenu.mostrarListado()
-				elif (rta2 == 1):
-					segundoMenu = Ventana.Ventana(path_exception_extension,
-											"slGuardian | Listado de Extensiones",
-											"Extensiones liberadas.",
-											formulario)
-					rta3 = segundoMenu.mostrarListado()
-				elif (rta2 == 2):
-					exit_sub_menu = True
-		elif (rta == 3):
-			logger.info('Herramientas del sistema.')
-			exit_sub_menu = False
-			while not exit_sub_menu:
-				auxMenu = Menu.Menu("slGuardian | Herramientas",
-								"Herramientas de Sistema",
-								("Configuraciones",
-								"Verificar Conexion",
-								"Volver"),
-								formulario)
-				rta2 = auxMenu.mostrarMenu()
-				if (rta2 == 0):
-					segundoMenu = Configuracion.VentanaConfiguracion(os.getcwd() + '/config/guardian.cnf',
-														'slGuardian | Configuracion',
-														'Configuración de Archivos',
-														formulario)
-					segundoMenu.modificarConfiguracion()
-				elif (rta2 == 1):
-					if (gw != ""):
-						ping.verbose_ping(gw)
-					ping.verbose_ping("google.com")
-				elif (rta2 == 2):
-					exit_sub_menu = True
-		elif (rta == 5):
+		elif (bb.buttonPressed(resultado) == "salir"):
 			logger.info('Finalización del sistema.')
-			salir = True
-		elif (rta == None):
-			logger.info('Finalización del sistema.')
-			salir = True
+			exit = True
 		else:
 			logger.error('Operación no válida.')
 
