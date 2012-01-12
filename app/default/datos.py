03b4a0f85c890c81b7635129249cd2f41feb0036
Daniel M. Maldonado <danyx@elcodigok.com.ar>
2012-01-12 12:37:18 -0300

    Cambios en el ingreso de tareas al Inbox

--- app/default/datos.py
+++ app/default/datos.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from datetime import date
+import gettext
 
 try:
 	import snack
@@ -17,23 +18,28 @@ class Datos:
 		self.screen = screen
 
 	def altas(self):
-		(boton, (nombre, descripcion)) = snack.EntryWindow(self.screen,
-														self.titulo,
-														self.texto,
-														['Nombre', 'Fecha'],
-														width=40,
-														entryWidth=30,
-														buttons=['Aceptar', 'Cancelar'])
+		sg = snack.Grid(2, 4)
+		boton = snack.ButtonBar(self.screen, ((gettext.gettext("Accept"), "accept"), (gettext.gettext("Back"), "back")))
+		descripcion = snack.Entry(30, "")
+		recordDate = snack.Entry(15, str(date.today().isoformat()))
+		textoLabel = snack.Label(self.texto)
+		sg.setField(snack.Label(gettext.gettext("Task")), 0, 0, padding = (0, 0, 2, 0), anchorLeft=1)
+		sg.setField(descripcion, 1, 0, anchorLeft = 1)
+		sg.setField(snack.Label(gettext.gettext("Date")), 0, 1, padding = (0, 1, 2, 0), anchorLeft=1)
+		sg.setField(recordDate, 1, 1, anchorLeft = 1, padding = (0, 1, 2, 0))
 		
-		if (descripcion != ""):
+		formAdd = snack.GridFormHelp(self.screen, self.titulo, help, 1, 3)
+		formAdd.add(sg, 0, 1, padding = (0, 0, 0, 1))
+		formAdd.add(boton, 0, 2, growx=1)
+		
+		resultado = formAdd.runOnce()
+		
+		if (boton.buttonPressed(resultado) == "accept"):
 			self.archivoEntrada = open(self.archivo, "a")
-			self.archivoEntrada.write(nombre)
-			self.archivoEntrada.write("\t:created" + date.today().isoformat())
+			self.archivoEntrada.write(descripcion.value())
+			self.archivoEntrada.write("\t:created" + recordDate.value())
 			self.archivoEntrada.write("\t:pNone"  + "\n")
-		else:
-			self.archivoEntrada = open(self.archivo, "a")
-			self.archivoEntrada.write(nombre + "\n")
-		self.archivoEntrada.close()
+			self.archivoEntrada.close()
 
 	def busqueda(self):
 		(boton, descripcion) = snack.EntryWindow(self.screen,
